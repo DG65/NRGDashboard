db842b4ab130632e7074fc35089c17512e08bfc3
DG65 <dietmar.gureth@me.com>
2026-08-16 21:42:23 +0200

    Vierten Wasserfluss-Stil "Fluessigkeitsglanz" ergaenzen
    Dietmar: "Hast Du noch andere Stile auf Lager? Vielleicht doch mal den
    Bender Stil?" - Anknuepfung an den urspruenglichen Wunsch nach einer
    fluessigeren Wasser-Textur wie in einer 3D-Fluidsimulation. Echte
    Fluiddynamik ist in reinem SVG/CSS nicht sinnvoll nachbildbar; ein
    breiter, stark verwaschener, langsam gleitender Glanzstreifen
    (stroke-width 5, blur 2px, lange weiche Segmente) simuliert
    stattdessen eine glaenzende, fliessende Oberflaeche statt einzelner
    Wassertropfen. Vierte Option im FlowStyle-Profil (Wert 3).

--- NRGDashboardHeatSchema/module.php
+++ NRGDashboardHeatSchema/module.php
@@ -107,6 +107,7 @@ class NRGDashboardHeatSchema extends IPSModule
         IPS_SetVariableProfileAssociation('NRGDASHHEAT.FlowStyle', 0, 'Organische Welle', '', -1);
         IPS_SetVariableProfileAssociation('NRGDASHHEAT.FlowStyle', 1, 'Gleichmäßige Punkte', '', -1);
         IPS_SetVariableProfileAssociation('NRGDASHHEAT.FlowStyle', 2, 'Fließende Striche', '', -1);
+        IPS_SetVariableProfileAssociation('NRGDASHHEAT.FlowStyle', 3, 'Flüssigkeitsglanz', '', -1);
         $flowIsNew = @IPS_GetObjectIDByIdent('FlowStyle', $this->InstanceID) === false;
         $this->RegisterVariableInteger('FlowStyle', 'Wasserfluss-Darstellung', 'NRGDASHHEAT.FlowStyle', 50);
         $this->EnableAction('FlowStyle');
